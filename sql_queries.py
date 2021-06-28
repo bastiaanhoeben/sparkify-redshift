5fd228e4213ee8e5faf642d177301b256bfe0ce0
Bastiaan Hoeben <76174738+bastiaanhoeben@users.noreply.github.com>
2021-06-28 10:24:19 +0200

    Add drop table and create table statements

--- sql_queries.py
+++ sql_queries.py
@@ -7,23 +7,59 @@ config.read('dwh.cfg')
 
 # DROP TABLES
 
-staging_events_table_drop = ""
-staging_songs_table_drop = ""
-songplay_table_drop = ""
-user_table_drop = ""
-song_table_drop = ""
-artist_table_drop = ""
-time_table_drop = ""
+staging_events_table_drop = "DROP TABLE IF EXISTS staging_events"
+staging_songs_table_drop = "DROP TABLE IF EXISTS staging_songs"
+songplay_table_drop = "DROP TABLE IF EXISTS songplays"
+user_table_drop = "DROP TABLE IF EXISTS users"
+song_table_drop = "DROP TABLE IF EXISTS songs"
+artist_table_drop = "DROP TABLE IF EXISTS artists"
+time_table_drop = "DROP TABLE IF EXISTS time"
 
 # CREATE TABLES
 
 staging_events_table_create= ("""
+CREATE TABLE staging_events (
+    artist VARCHAR
+    auth VARCHAR
+    firstName VARCHAR 
+    gender VARCHAR(1)
+    ItemInSession INT
+    lastName VARCHAR
+    lenght FLOAT
+    level VARCHAR
+    location VARCHAR
+    method VARCHAR
+    page VARCHAR
+    registration FLOAT
+    sessionId INT
+    song VARCHAR
+    status INT
+    ts INT
+    userAgent VARCHAR
+    userID INT
 """)
 
 staging_songs_table_create = ("""
+CREATE TABLE staging_songs (
+    num_songs INT
+    artist_id VARCHAR
+    artist_latitude FLOAT
+    artist_location VARCHAR
+    artist_name VARCHAR
+    song_id VARCHAR
+    title VARCHAR
+    duration FLOAT
+    year INT
+)
 """)
 
 songplay_table_create = ("""
+CREATE TABLE songplays (
+    songplay_id IDENTITY(0,1) PRIMARY KEY
+    start_time INT FOREIGN KEY REFERENCES time (start_time)
+    user_id INT FOREIGN KEY REFERENCES users (user_id)
+    
+)
 """)
 
 user_table_create = ("""
@@ -41,6 +77,9 @@ time_table_create = ("""
 # STAGING TABLES
 
 staging_events_copy = ("""
+COPY {} FROM 's3://udacity-dend/log_data'
+CREDENTIALS 'aws_iam_role={}'
+json region 'us-west-2'
 """).format()
 
 staging_songs_copy = ("""
