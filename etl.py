2b10ba4ef2f926c2c6fd663d4b6259679d98d45c
Bastiaan Hoeben <76174738+bastiaanhoeben@users.noreply.github.com>
2021-07-06 09:04:47 +0200

    Load config parameters into database connection

--- etl.py
+++ etl.py
@@ -1,6 +1,7 @@
 import configparser
 import psycopg2
 from sql_queries import copy_table_queries, insert_table_queries
+from aws_config import get_cluster_endpoint
 
 
 def load_staging_tables(cur, conn):
@@ -19,7 +20,16 @@ def main():
     config = configparser.ConfigParser()
     config.read('dwh.cfg')
 
-    conn = psycopg2.connect("host={} dbname={} user={} password={} port={}".format(*config['CLUSTER'].values()))
+    KEY=config.get('AWS', 'KEY')
+    SECRET=config.get('AWS', 'SECRET')
+    REGION=config.get('AWS', 'REGION')
+    DWH_CLUSTER_IDENTIFIER=config.get('DWH', 'DWH_CLUSTER_IDENTIFIER')
+
+    DB_NAME, DB_USER, DB_PASSWORD, DB_PORT = config['DB'].values()
+
+    HOST=get_cluster_endpoint(REGION, KEY, SECRET, DWH_CLUSTER_IDENTIFIER)
+
+    conn = psycopg2.connect(f"host={HOST} dbname={DB_NAME} user={DB_USER} password={DB_PASSWORD} port={DB_PORT}")
     cur = conn.cursor()
     
     load_staging_tables(cur, conn)
