93c75eb8b14b37e67cd3c7161b1758318dd3105b
Bastiaan Hoeben <76174738+bastiaanhoeben@users.noreply.github.com>
2021-07-06 11:49:08 +0200

    Rename host parameter

--- create_tables.py
+++ create_tables.py
@@ -27,9 +27,9 @@ def main():
 
     DB_NAME, DB_USER, DB_PASSWORD, DB_PORT = config['DB'].values()
 
-    HOST=get_cluster_endpoint(REGION, KEY, SECRET, DWH_CLUSTER_IDENTIFIER)
+    host=get_cluster_endpoint(REGION, KEY, SECRET, DWH_CLUSTER_IDENTIFIER)
 
-    conn = psycopg2.connect(f"host={HOST} dbname={DB_NAME} user={DB_USER} password={DB_PASSWORD} port={DB_PORT}")
+    conn = psycopg2.connect(f"host={host} dbname={DB_NAME} user={DB_USER} password={DB_PASSWORD} port={DB_PORT}")
     cur = conn.cursor()
 
     drop_tables(cur, conn)
